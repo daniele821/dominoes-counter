176e939fc8558b07b2f86c38d2f5cc6469969b7a
- <->
2025-08-13 14:38:29 +0200

    added colors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use std::fmt::Display;
 
 const STR_HALFBLOCK: &str = "▀";
+const COL_CLEAN: &str = "\x1b[0m";
+const COL_BOLD: &str = "\x1b[1m";
+const COL_RED: &str = "\x1b[31m";
+const COL_LGREEN: &str = "\x1b[32m";
+const COL_YELLOW: &str = "\x1b[33m";
+const COL_BLUE: &str = "\x1b[34m";
+const COL_PURPLE: &str = "\x1b[35m";
+const COL_GREEN: &str = "\x1b[36m";
+const COL_WHITE: &str = "\x1b[37m";
 
 #[derive(Debug, Default)]
 enum DominoCell {
@@ -47,7 +56,7 @@ impl Display for DominoArea {
 
 fn main() {
     let i = DominoArea::create_empty(3, 5);
-    println!("{}", i.to_string());
+    println!("{}{}{}{}", COL_RED, COL_BOLD, i.to_string(), COL_CLEAN);
 }
 
 #[cfg(test)]
