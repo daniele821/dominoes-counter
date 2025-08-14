0c78e5b4bec27e7a9c76108ad7320c0f7e34366d
- <->
2025-08-14 14:32:44 +0200

    simplify DominoArea by keeping highlight outside of it (more complex custom_fmt function)

--- src/domino.rs
+++ src/domino.rs
@@ -1,7 +1,6 @@
-use std::fmt::Display;
-
 const STR_BLOCK: &str = "  ";
-const STR_BLOCK_HIGHLIGHT: &str = "\x1b[1;30m✪ ";
+const STR_BLOCK_HIGHLIGHT: &str = "✪ ";
+const COL_TEXT: &str = "\x1b[1;30m";
 const COL_CLEAN: &str = "\x1b[0m";
 const COL_WHITE: &str = "\x1b[1;47m";
 const COL_RED: &str = "\x1b[1;41m";
@@ -25,7 +24,6 @@ pub struct DominoArea {
     rows: u64,
     cols: u64,
     cells: Vec<DominoColor>,
-    highlight: Vec<u64>,
 }
 
 impl DominoArea {
@@ -34,7 +32,6 @@ impl DominoArea {
             rows,
             cols,
             cells: (0..rows * cols).map(|_| DominoColor::Empty).collect(),
-            highlight: vec![],
         }
     }
 
@@ -79,10 +76,6 @@ impl DominoArea {
         self.get_cell_at_index_mut(self.to_index(row, col))
     }
 
-    pub fn get_highlight_mut(&mut self) -> &mut Vec<u64> {
-        &mut self.highlight
-    }
-
     pub fn get_valid_colors(&self, indexes: &[u64]) -> Vec<DominoColor> {
         for index in indexes {
             let cell = self.get_cell_at_index(*index);
@@ -122,10 +115,6 @@ impl DominoArea {
     pub fn compute_empty_nears(&self) -> Vec<u64> {
         let mut result = Vec::<u64>::with_capacity(self.cells.len());
         for index in 0..self.cells.len() {
-            if *self.get_cell_at_index(u64::try_from(index).unwrap()) != DominoColor::Empty {
-                result.push(0);
-                continue;
-            }
             let row = self.row_from_index(u64::try_from(index).unwrap());
             let col = self.col_from_index(u64::try_from(index).unwrap());
             let empty_nears = self
@@ -137,28 +126,34 @@ impl DominoArea {
         }
         result
     }
-}
 
-impl Display for DominoArea {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    pub fn custom_fmt(&self, highlights: &[u64], custom: &[String]) -> String {
+        let mut fmt = Vec::<String>::new();
         for row in 0..self.rows {
             for col in 0..self.cols {
                 let mut block = STR_BLOCK;
-                if self.highlight.contains(&self.to_index(row, col)) {
+                if highlights.contains(&self.to_index(row, col)) {
                     block = STR_BLOCK_HIGHLIGHT;
                 }
-                match self.get_cell(row, col) {
-                    DominoColor::Empty => write!(f, "{}{}{}", COL_WHITE, block, COL_CLEAN)?,
-                    DominoColor::Red => write!(f, "{}{}{}", COL_RED, block, COL_CLEAN)?,
-                    DominoColor::Green => write!(f, "{}{}{}", COL_GREEN, block, COL_CLEAN)?,
-                    DominoColor::Yellow => write!(f, "{}{}{}", COL_YELLOW, block, COL_CLEAN)?,
-                    DominoColor::Blue => write!(f, "{}{}{}", COL_BLUE, block, COL_CLEAN)?,
-                    DominoColor::Unused => write!(f, "{}{}{}", COL_BLACK, block, COL_CLEAN)?,
+                if let Some(text) = custom.get(usize::try_from(self.to_index(row, col)).unwrap())
+                    && !text.is_empty()
+                {
+                    block = text;
                 }
+                assert_eq!(2, block.chars().count());
+                let shared_part = format!("{COL_TEXT}{block}{COL_CLEAN}");
+                fmt.push(match self.get_cell(row, col) {
+                    DominoColor::Empty => format!("{}{}", COL_WHITE, shared_part),
+                    DominoColor::Red => format!("{}{}", COL_RED, shared_part),
+                    DominoColor::Green => format!("{}{}", COL_GREEN, shared_part),
+                    DominoColor::Yellow => format!("{}{}", COL_YELLOW, shared_part),
+                    DominoColor::Blue => format!("{}{}", COL_BLUE, shared_part),
+                    DominoColor::Unused => format!("{}{}", COL_BLACK, shared_part),
+                })
             }
-            write!(f, "\n")?;
+            fmt.push(format!("\n"));
         }
-        Ok(())
+        fmt.join("")
     }
 }
 
@@ -171,7 +166,7 @@ mod tests {
         let mut domino_area = DominoArea::create_empty(3, 5);
         domino_area.cells[12] = DominoColor::Unused;
         domino_area.cells[13] = DominoColor::Unused;
-        println!("\n{domino_area}");
+        println!("\n{}", domino_area.custom_fmt(&[], &[]));
 
         let indexes = [
             vec![1, 2, 3],
@@ -183,9 +178,8 @@ mod tests {
         for index in indexes {
             let valid_colors = domino_area.get_valid_colors(&index);
             println!("{valid_colors:?}");
-            *domino_area.get_highlight_mut() = index.clone();
             domino_area.set_valid_color(&index, valid_colors[0].clone());
-            println!("{domino_area}");
+            println!("{}", domino_area.custom_fmt(&index, &[]));
         }
     }
 
@@ -214,25 +208,36 @@ mod tests {
 
     #[test]
     pub fn test_empty_nears() {
-        let mut domino_area = DominoArea::create_empty(3, 3);
+        let mut domino_area = DominoArea::create_empty(3, 4);
+        *domino_area.get_cell_mut(0, 3) = DominoColor::Unused;
+        *domino_area.get_cell_mut(2, 3) = DominoColor::Unused;
+        println!("\n{}", domino_area.custom_fmt(&[], &[]));
 
         let tests = [
-            (vec![2, 3, 2, 3, 4, 3, 2, 3, 2], vec![0, 1]),
-            (vec![0, 0, 1, 2, 3, 3, 2, 3, 2], vec![2, 5]),
-            (vec![0, 0, 0, 2, 2, 0, 2, 3, 1], vec![7, 8]),
-            (vec![0, 0, 0, 2, 1, 0, 1, 0, 0], vec![3, 6]),
-            (vec![0, 0, 0, 0, 0, 0, 0, 0, 0], vec![4]),
-            (vec![0, 0, 0, 0, 0, 0, 0, 0, 0], vec![]),
+            (vec![1, 2, 1, 2, 2, 3, 4, 1, 2, 3, 2, 2], vec![0,1])
         ];
 
         for test in tests {
+            let colors = domino_area.get_valid_colors(&test.1);
+            domino_area.set_valid_color(&test.1, colors[0].clone());
             let expected = test.0;
             let actual = domino_area.compute_empty_nears();
             assert_eq!(expected, actual);
             println!("{actual:?}");
-            println!("{domino_area}");
-            let colors = domino_area.get_valid_colors(&test.1);
-            domino_area.set_valid_color(&test.1, colors[0].clone());
+            let custom_text: Vec<_> = actual
+                .iter()
+                .enumerate()
+                .map(|(i, n)| {
+                    if *domino_area.get_cell_at_index(u64::try_from(i).unwrap())
+                        == DominoColor::Empty
+                    {
+                        format!("{n:<2}")
+                    } else {
+                        format!("")
+                    }
+                })
+                .collect();
+            println!("{}", domino_area.custom_fmt(&test.1, &custom_text));
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,1 @@
-use dominoes_counter::domino::{DominoArea, DominoColor};
-
-fn main() {
-    let mut domino_area = DominoArea::create_empty(3, 5);
-    *domino_area.get_cell_at_index_mut(4) = DominoColor::Unused;
-    *domino_area.get_cell_at_index_mut(9) = DominoColor::Unused;
-    *domino_area.get_cell_at_index_mut(14) = DominoColor::Unused;
-    println!("\n{domino_area}");
-
-    let indexes = [[1, 2], [0, 5], [10, 11], [6, 7], [12, 13], [3, 8]];
-    for index in indexes {
-        let valid_colors = domino_area.get_valid_colors(&index);
-        println!("{valid_colors:?}");
-        *domino_area.get_highlight_mut() = index.to_vec();
-        domino_area.set_valid_color(&index, valid_colors[0].clone());
-        println!("{domino_area}");
-    }
-}
+fn main() {}
